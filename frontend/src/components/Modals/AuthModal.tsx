ae6ee8f69b2620f81c2dde1276bf2984f982096b
hoangtv090103 <hoangtv090103@gmail.com>
2024-05-23 11:21:27 +0700

    Refactor AuthModal and Signup components, and update recoil state management

--- frontend/src/components/Modals/AuthModal.tsx
+++ frontend/src/components/Modals/AuthModal.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 import { Login } from "./Login";
 import Signup from "./Signup";
@@ -10,14 +10,14 @@ export type IAuthModalProps = {};
 
 export default function AuthModal(props: IAuthModalProps) {
   const authModal = useRecoilValue(authModalState);
-  const setAuthModalState = useSetRecoilState(authModalState);
 
-  const handleClose = () => {
-    setAuthModalState((prev) => ({ ...prev, isOpen: false }));
-  };
+  const closeModal = useCloseModal();
   return (
     <div>
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"></div>
+      <div
+        className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"
+        onClick={closeModal}
+      ></div>
       <div className="w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
         <div className="relative w-full h-full mx-auto flex items-center justify-center">
           <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6">
@@ -25,7 +25,7 @@ export default function AuthModal(props: IAuthModalProps) {
               <button
                 type="button"
                 className="bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white"
-                onClick={handleClose}
+                onClick={closeModal}
               >
                 <IoClose className="h-5 w-5" />
               </button>
@@ -42,4 +42,21 @@ export default function AuthModal(props: IAuthModalProps) {
       </div>
     </div>
   );
+}
+
+function useCloseModal() {
+  const setAuthModal = useSetRecoilState(authModalState);
+
+  const closeModal = () => {
+    setAuthModal((prev) => ({ ...prev, isOpen: false, type: "login" }));
+  };
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") closeModal();
+    };
+
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
+  return closeModal;
 }
--- frontend/src/components/Modals/Signup.tsx
+++ frontend/src/components/Modals/Signup.tsx
@@ -1,12 +1,20 @@
+import { authModalState } from "@/atoms/authModalAtom";
 import React from "react";
+import { useSetRecoilState } from "recoil";
 
 type SignupProps = {};
 
 const Signup: React.FC<SignupProps> = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const handleClick = (type: "login" | "signup" | "resetPassword") => {
+    setAuthModalState((prev) => ({ ...prev, type }));
+  };
   return (
     <div>
       <form className="space-y-6 px-6 py-5">
-        <h3 className="text-x; font-medium text-white">Register to LeetsCode</h3>
+        <h3 className="text-x; font-medium text-white">
+          Register to LeetsCode
+        </h3>
         <div>
           <label
             htmlFor="email"
@@ -53,13 +61,18 @@ const Signup: React.FC<SignupProps> = () => {
         <button
           type="submit"
           className="w-full text-white focus:ring-blue-300 font-medium rounder-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s"
+          onClick={() => handleClick("signup")}
         >
           Register
         </button>
 
         <div className="text-sm font-medium text-gray-300">
           Alreay have an account?{" "}
-          <a href="#" className="text-blue-700 hover:underline">
+          <a
+            href="#"
+            className="text-blue-700 hover:underline"
+            onClick={() => handleClick("login")}
+          >
             Login
           </a>
         </div>
